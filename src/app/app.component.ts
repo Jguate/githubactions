70150ed513f553991a6ae69f25bb40268751407e
Javier Cano <javier.cano@myappsoftware.com>
2022-02-03 15:02:20 -0600

    Main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipelinegithub';
+
+  ngOnInit(){
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Hola actualizado desde aws pipeline',
+      showConfirmButton: false,
+      timer: 5000
+    })
+  }
 }
